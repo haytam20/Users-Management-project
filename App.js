cf34038444d714f94344e19e983036d74dafe24c
hello World <talbi20h@gmail.com>
2025-04-14 16:15:06 +0100

    add connection to db

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3002
+const mongoose=require('mongoose')
 
 app.get('/', (req, res) => {
   res.sendFile("./Home.html",{root:__dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+
+mongoose
+    .connect("mongodb+srv://haytam1331:bOFbsZlIOBnoGSAY@cluster0.5uydnqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(()=>{
+        app.listen(port, () => {
+            console.log(`http://localhost:${port}/`)
+          })
+    })
+    .catch((err)=>{console.log(err)});
+
+
+
+    // bOFbsZlIOBnoGSAY
