9c9cb83dbfffa9ed77e7682ad979dfad8b0a421f
hello World <talbi20h@gmail.com>
2025-04-15 14:12:27 +0100

    seend to db

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3002
-const mongoose=require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const port = 3002;
+app.use(express.urlencoded({ extended: true }));
+// Import du modèle
+const Article = require('./models/Text');
 
+// Middleware pour parser les données des formulaires
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Affiche la page d'accueil
 app.get('/', (req, res) => {
-  res.sendFile("./Home.html",{root:__dirname})
-})
+  res.sendFile(path.join(__dirname, 'Home.html'));
+});
 
+// Reçoit les données du formulaire
+app.post('/add-text', (req, res) => {
+  const article = new Article({
+    text: req.body.text
+  });
 
-
-mongoose
-    .connect("mongodb+srv://haytam1331:bOFbsZlIOBnoGSAY@cluster0.5uydnqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=>{
-        app.listen(port, () => {
-            console.log(`http://localhost:${port}/`)
-          })
+  article
+    .save()
+    .then(result => {
+      console.log('Article enregistré:', result);
+      res.redirect('/');
     })
-    .catch((err)=>{console.log(err)});
+    .catch(err => {
+      console.error('Erreur enregistrement article:', err);
+      res.status(500).send('Erreur serveur');
+    });
+});
 
-
-
-    // bOFbsZlIOBnoGSAY
+// Connexion à MongoDB et lancement du serveur
+mongoose
+  .connect("mongodb+srv://haytam1331:bOFbsZlIOBnoGSAY@cluster0.5uydnqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Serveur en cours d'exécution : http://localhost:${port}/`);
+    });
+  })
+  .catch(err => {
+    console.error('Erreur de connexion MongoDB :', err);
+  });
