ca3172fda6ceccadd2dc08837f239703a9cfaa22
hello World <talbi20h@gmail.com>
2025-04-16 14:05:58 +0100

    auto reload

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
+const path = require('path'); 
 const app = express();
 const port = 3002;
-app.use(express.urlencoded({ extended: true }));
-// Import du modèle
-const Article = require('./models/Text');
 
-// Middleware pour parser les données des formulaires
+app.use(express.static('public'));
+
+const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+// Auto refresh
+const connectLivereload = require('connect-livereload');
+app.use(connectLivereload());
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Affiche la page d'accueil
+// EJS setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Import model
+const Article = require('./models/Text');
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Home.html'));
+  res.render('Home');
 });
 
-// Reçoit les données du formulaire
 app.post('/add-text', (req, res) => {
   const article = new Article({
-    text: req.body.text
+    text: req.body.text,
   });
 
   article
@@ -34,9 +53,11 @@ app.post('/add-text', (req, res) => {
     });
 });
 
-// Connexion à MongoDB et lancement du serveur
+// MongoDB connection and server start
 mongoose
-  .connect("mongodb+srv://haytam1331:bOFbsZlIOBnoGSAY@cluster0.5uydnqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(
+    'mongodb+srv://haytam1331:bOFbsZlIOBnoGSAY@cluster0.5uydnqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
   .then(() => {
     app.listen(port, () => {
       console.log(`Serveur en cours d'exécution : http://localhost:${port}/`);
